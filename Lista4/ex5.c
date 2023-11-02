e214b4dc7f8c0d48996a34715634d0682c0f2f96
luisastellet <luisastellet@id.uff.br>
2023-11-02 19:48:15 -0300

    .

--- Lista4/ex5.c
+++ Lista4/ex5.c
@@ -2,4 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-int cic (char *str1, char *str2){}
+void minusculo(char *str){
+    for(int i=0; i<strlen(str); i++){
+        if((int)str[i] >= 65 && (int)str[i] <= 90) str[i] = (char)((int)str[i]+32);
+    }
+}
+
+int cic (char *str1, char *str2){
+    minusculo(str1);
+    minusculo(str2);
+    int inicio = -1;
+
+    for(int i=0; i<strlen(str2); i++){
+        int ver=1;
+        if(str2[i] == str1[0]){
+            inicio=i+1;
+            for(int j=1; j<strlen(str2); j++){ //indice da pequena
+                if((int)str1[j] != (int)str2[inicio]){
+                    ver = 0; //deu merda
+                    break;
+                }
+                inicio++;
+            }
+            if(ver==1) return 1;
+        } 
+    }
+    return 0;
+}
+
+int main(){
+    char string1[20], string2[20];
+    printf("Digite a string 1: ");
+    scanf("%s", string1);
+    printf("Digite a string 2: ");
+    scanf("%s", string2);
+
+    printf("%d", cic(string1, string2));
+
+}
